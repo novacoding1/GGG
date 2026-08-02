6ab65523dd2b69beb559348ea556067af05926d6
novacoding1 <qweteroiyn@gmail.com>
2026-08-02 23:49:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GGG/', // 👈 добавь эту строку
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 3000,
     host: true,
